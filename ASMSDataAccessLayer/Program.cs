eb0096fed08363280da8c06f88df6e926b0ba23b
sgorkemcil <sgorkemcil@gmail.com>
2022-04-26 12:16:03 +0300

    Add project files.

---
+++ ASMSDataAccessLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASMSDataAccessLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ASMSEntityLayer/Enums/AllEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSEntityLayer.Enums
+{
+    public class AllEnums
+    {
+
+    }
+    public enum Genders : byte 
+    {
+        Male=0,
+        Female=1,
+        Unkown=2
+    }
+}
